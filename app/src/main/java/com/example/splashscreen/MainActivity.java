c135aa026448858fa3985ba858412069865f6a71
Mansurzoda <bhj@mail.com>
2022-12-02 14:09:45 +0300

    screen

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run(){
+
+            }
+        }
+    }
+
+
+
+}
